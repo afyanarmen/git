ae123a5a28276cce23422c2052a8df70df92294e
Your Name <you@example.com>
2018-03-28 17:02:28 +0400

    first

---
+++ git/js.js
@@ -0,0 +1,10 @@
+
+var myInput = document.getElementById("pass");
+function Function() {
+
+if (pass == "Nare") {
+  document.location.href = "html.html";
+} else {
+    document.location.href = "html.html";
+}
+};
